49a61fd6bcf894bdfec5a61c73268f455afc7537
ConfusedGerman <111304029+ConfusedGerman@users.noreply.github.com>
2023-08-11 17:42:18 +0000

    Implement forms in javascript, build function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-test
+const password1 = document.getElementById("user_password");
+const password2 = document.getElementById("user_password_conformation");
+const checkButton = document.getElementById("submit_button");
+
+checkButton.addEventListener("click", () => {
+    if (password1.value === password2.value) {
+    } else {
+    }
+});
