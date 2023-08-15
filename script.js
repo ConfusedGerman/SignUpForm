ab35766212e9a20c779920444548d3ac9b94beec
ConfusedGerman <okehermanns11@gmail.com>
2023-08-15 21:56:41 +0200

    Add error message if passwords do not match

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const password1 = document.getElementById("user_password");
-const password2 = document.getElementById("user_password_conformation");
-const checkButton = document.getElementById("submit_button");
-
-checkButton.addEventListener("click", () => {
-    if (password1.value === password2.value) {
-    } else {
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    const password1 = document.getElementById("user_password");
+    const password2 = document.getElementById("user_password_conformation");
+    
+    password2.addEventListener("input", function() {
+        if (password1.value !== password2.value) {
+            password2.setCustomValidity("Passwords do not match.");
+        } else {
+            password2.setCustomValidity("");
+        }
+    });
 });
